f955206bb6b1b22462e93ffdb26089d2e33372ca
NeoYung <79010365@qq.com>
2023-06-18 18:38:39 +0800

    test: add type test

---
+++ test/type.test.ts
@@ -0,0 +1,32 @@
+import { describe, expectTypeOf, test } from 'vitest'
+import { asyncFlow } from '../src/asyncFlow.js'
+import { asyncPipe } from '../src/asyncPipe.js'
+import { asyncReduce } from '../src/asyncReduce.js'
+
+describe('test all fn return type', () => {
+  test('asyncFlow', async () => {
+    const loadInitialValue = async () => Promise.resolve(1)
+    const syncTail = (x: number) => x + 1
+    const result = asyncFlow(loadInitialValue, syncTail)
+    const asyncTail = (x: number) => Promise.resolve(x)
+    const result2 = asyncFlow(loadInitialValue, asyncTail)
+    expectTypeOf(result).toMatchTypeOf<() => Promise<number>>()
+    expectTypeOf(result2).toMatchTypeOf<() => Promise<number>>()
+  })
+
+  test('asyncPipe', async () => {
+    const syncTail = (x: number) => x + 1
+    const result = asyncPipe(Promise.resolve(1), syncTail)
+    const asyncTail = (x: number) => Promise.resolve(x)
+    const result2 = asyncPipe(Promise.resolve(1), asyncTail)
+    expectTypeOf(result).toMatchTypeOf<Promise<number>>()
+    expectTypeOf(result2).toMatchTypeOf<Promise<number>>()
+  })
+
+  test('asyncPipe', async () => {
+    const loadInitialValue = async () => Promise.resolve(1)
+    const asyncTail = (x: number) => Promise.resolve(x)
+    const result2 = asyncReduce([loadInitialValue, asyncTail], 0)
+    expectTypeOf(result2).toMatchTypeOf<Promise<number>>()
+  })
+})
