488fe70a67447e3ba02accdd2bd23a6aefdeb999
NeoYung <79010365@qq.com>
2023-07-12 23:14:23 +0800

    refector: remove unexport function

--- src/asyncReduce.ts
+++
@@ -1,9 +0,0 @@
-type AsyncReduceFn = <T>(
-  fns: ((x: T) => Promise<T>)[],
-  initialValue: T
-) => Promise<T>
-
-export const asyncReduce: AsyncReduceFn = (fns, initial) => {
-  const [first, ...rest] = fns
-  return rest.reduce((acc, fn) => acc.then(fn), first(initial))
-}
