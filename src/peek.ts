d0607ad801e39131a2d47a2df444fa630a75744a
NeoYung <79010365@qq.com>
2023-07-12 23:16:50 +0800

    fix: lossing type when pass console.log

--- src/peek.ts
+++ src/peek.ts
@@ -1,6 +1,11 @@
 /**
  * peek value only
+ * ## Attention
+ * this function will have breaking change\
+ * on next major version\
+ * no more return function and call console.log inside\
+ * use `invoke` instead
  */
 export const peek =
-  <T>(fn: (x: T) => void) =>
-  (x: T) => (fn(x), x)
+  <T, P extends T>(fn: (x: P) => void) =>
+  (x: T) => (fn(x as P), x)
